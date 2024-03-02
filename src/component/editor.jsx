48de257481f7e9d72c810a73d5a919bf8eba6b50
Shivesh Gupta <shivesh@buyhatke.com>
2024-03-02 16:00:28 +0530

    update code for logo

--- src/component/editor.jsx
+++ src/component/editor.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 class SwagEditor extends Component {
   state = {
     player: 0,
@@ -10,11 +9,11 @@ class SwagEditor extends Component {
     canvasY: 400,
     canvasX: 400,
     start: true,
-    rounded: false,
+    rounded: 0,
     cornerValue: 0,
     customSize: false,
     scaleSize: 100,
-    backgroundColor: 'pink',
+    backgroundColor: '#2f064b',
     textColor: 'white',
   };
   imageSize = {
@@ -27,21 +26,29 @@ class SwagEditor extends Component {
     custom: { x: 500, y: 500 },
   };
 
+  // constructor(props) {
+  //   super(props);
+  //   this.canvasRef = React.createRef();
+  // }
+
   componentDidMount() {
     this.clearCanvas();
     const { canvasX, canvasY } = this.state;
     var canvas = document.getElementById("backgroundCanvas");
-    canvas.width = canvasX;
-    canvas.height = canvasY;
-    console.log("assa")
+    if (canvas) {
+      canvas.width = canvasX;
+      canvas.height = canvasY;
+    }
   }
   componentDidUpdate() {
     const { canvasX, canvasY, start } = this.state;
     var canvas = document.getElementById("backgroundCanvas");
-    canvas.width = canvasX;
-    canvas.height = canvasY;
-    start && this.setState({ start: false });
-    this.addBackground();
+    if (canvas) {
+      canvas.width = canvasX;
+      canvas.height = canvasY;
+      start && this.setState({ start: false });
+      this.addBackground();
+    }
   }
 
   handle = (e) => {
@@ -72,14 +79,6 @@ class SwagEditor extends Component {
       canvasY: y < 60 ? 500 : y,
       customSize: false,
     });
-  };
-
-  handleFrame = (e) => {
-    let { name, value } = e.target;
-    document.getElementById(
-      name
-    ).src = `./assests/img/background/bg (${value}).png`;
-    this.setState({ frame: value });
   };
 
   playerUpdate = () => {
@@ -137,13 +136,13 @@ class SwagEditor extends Component {
     }
   }
 
-  playerSelect = (e) => {
-    let { name, value } = e.target;
-    document.getElementById(
-      name
-    ).src = `./assests/img/players/player (${value}).svg`;
-    this.setState({ player: value });
-  };
+  // playerSelect = (e) => {
+  //   let { name, value } = e.target;
+  //   document.getElementById(
+  //     name
+  //   ).src = `./swageditor/assests/img/players/player (${value}).svg`;
+  //   this.setState({ player: value });
+  // };
 
   download_img = (e) => {
     this.addBackground();
@@ -153,55 +152,72 @@ class SwagEditor extends Component {
 
   };
 
-  roundedCorner = () => {
-    const { canvasX, canvasY } = this.state;
-    var canvas = document.getElementById("backgroundCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(canvasX / 2, canvasY / 2, canvasX / 2, 0, 2 * Math.PI);
-    ctx.clip();
-    // ctx.stroke();
-    ctx.closePath();
-    return true;
+  roundedCorner = (e) => {
+    this.clearCanvas();
+    let { value } = e.target;
+    this.setState({ rounded: value || 0 }, () => {
+      this.addBackground();
+    });
   };
 
   addBackground = () => {
-    const { canvasX, canvasY, rounded } = this.state;
+    const { canvasX, canvasY, rounded = 2 } = this.state;
     this.clearCanvas();
     var canvas = document.getElementById("backgroundCanvas");
-    var ctx = canvas.getContext("2d");
-    var img = document.getElementById("backgoundImg");
-    rounded && this.roundedCorner();
+    if (canvas) {
+      var ctx = canvas.getContext("2d");
+      var img = document.getElementById("backgoundImg");
+      if (rounded > 1) {
+        if (rounded > 200) {
+          var centerX = canvas.width / 2;
+          var centerY = canvas.height / 2;
+          var radius = Math.min(centerX, centerY);
 
-    const src = img.src.charAt(img.src.length - 1);
-    src !== "#" &&
-      ctx.drawImage(img, 0, 0, canvasX, canvasY);
-    ctx.imageSmoothingEnabled = false;
-    this.addFrame();
-    return true;
-  };
+          ctx.beginPath();
+          ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+          ctx.stroke();
+        }
+        else {
+          var borderRadius = rounded;
+          var width = img.width;
+          var height = img.height;
+          var x = (canvas.width - width) / 2;
+          var y = (canvas.height - height) / 2;
 
-  addFrame = () => {
-    const { canvasX, canvasY } = this.state;
-    var canvas = document.getElementById("backgroundCanvas");
-    var ctx = canvas.getContext("2d");
-    var img = document.getElementById("frameImg");
-    const src = img.src.charAt(img.src.length - 1);
-    src !== "#" &&
-      ctx.drawImage(img, 0, 0, canvasX, canvasY);
-    this.addPlayer();
+          ctx.beginPath();
+          ctx.moveTo(x + borderRadius, y);
+          ctx.arcTo(x + width, y, x + width, y + height, borderRadius);
+          ctx.arcTo(x + width, y + height, x, y + height, borderRadius);
+          ctx.arcTo(x, y + height, x, y, borderRadius);
+          ctx.arcTo(x, y, x + width, y, borderRadius);
+          ctx.closePath();
+        }
+        ctx.clip();
+      }
+      const src = img.src.charAt(img.src.length - 1);
+      if (src) {
+        src !== "#" &&
+          ctx.drawImage(img, 0, 0, canvasX, canvasY);
+        ctx.imageSmoothingEnabled = false;
+        this.addPlayer();
+      }
+    }
     return true;
   };
 
   addPlayer = () => {
     const { px, py, canvasX, canvasY } = this.state;
     var canvas = document.getElementById("backgroundCanvas");
-    var ctx = canvas.getContext("2d");
-    var img = document.getElementById("playerImg");
-    let src = img.src.charAt(img.src.length - 1);
-    const sizeImg = (Math.min(canvasX, canvasY) * 20) / 100;
-    src !== "#" &&
-      ctx.drawImage(img, px, py, sizeImg, sizeImg);
+    if (canvas) {
+      var ctx = canvas.getContext("2d");
+      var img = document.getElementById("playerImg");
+      let src = img.src.charAt(img.src.length - 1);
+      if (src) {
+        const sizeImg = (Math.min(canvasX, canvasY) * 20) / 100;
+        src !== "#" &&
+          ctx.drawImage(img, px, py, sizeImg, sizeImg);
+      }
+    }
     return true;
   };
 
@@ -221,16 +237,15 @@ class SwagEditor extends Component {
 
         </div> */}
         <div className="p-4 px-2 w-screen min-h-screen relative text-center" style={customStyle[0]}>
-          <section className="flex justify-center p-4 py-6">
-            <img src="./assests/aliando-rocky.regular.png" />
+          <section className="flex justify-center px-4 pt-6 text-2xl md:text-4xl">
+            {/* <img src="./swagEditor/assests/aliando-rocky.regular.png" alt="logo" /> */}
+            Marketing Profile Icon
           </section>
-          <section className="flex flex-col md:flex-row flex-wrap justify-center">
-            {/* file background */}
-
-            <section className="flex justify-between bg-red-400">
-
-              <span className="bg-red-600 p-3 w-full">Select a file</span>
-              <label className="bg-red-400 hoverBg flex flex-col items-center px-4 py-2 bg-transparent  cursor-pointer outline-none focus:outline-none  hover:text-blue-100">
+          <p className="text-red-500 mb-6 font-medium">Note: If the image does not update, click the 'Add | Update Design' button.</p>
+          <section className="flex flex-col md:flex-row flex-wrap gap-2 justify-center">
+            <section className="flex bg-purple-800">
+              <span className="bg-purple-900 p-3 min-w-200 text-left">Select a file</span>
+              <label className="bg-purple-800 hoverBg flex flex-col items-center px-4 py-2 bg-transparent  cursor-pointer outline-none focus:outline-none  hover:text-blue-100 w-full">
                 <svg
                   className="w-8 h-8"
                   fill="currentColor"
@@ -257,35 +272,13 @@ class SwagEditor extends Component {
                   className="hidden"
                 />
               </label>
-
             </section>
 
-            <section className="flex justify-between bg-red-400">
-              {/* Players */}
-
-
-              <span className="bg-red-600 p-3">Players</span>
-              <select
-                onChange={this.playerSelect}
-                name="playerImg"
-                className=" bg-red-400 p-3"
-              >
-                <option value="1">Bhuvneshwar </option>
-                <option value="2">Hardik</option>
-                <option value="3">Hardik-2</option>
-                <option value="4">Jasprit</option>
-                <option value="5">Kieron </option>
-                <option value="6">Rohit </option>
-                <option value="7">Rohit-2</option>
-                <option value="8">Shikhar </option>
-                <option value="9">Virat </option>
-                <option value="10">Yuvraj </option>
-              </select>
-
-
-              {/* frames */}
-              <div className="flex bg-red-400 hoverBg">
-                <label className="flex flex-col items-center px-4 py-2  bg-transparent  cursor-pointer outline-none focus:outline-none  hover:text-blue-100">
+            <section className="flex bg-purple-800">
+              <span className="bg-purple-900 p-3 min-w-200 text-left">Logo</span>
+             
+              <div className="flex bg-purple-800 hoverBg w-full">
+                <label className="flex flex-col items-center px-4 py-2  bg-transparent  cursor-pointer outline-none focus:outline-none  hover:text-blue-100 w-full">
                   <svg
                     className="w-8 h-8"
                     fill="currentColor"
@@ -306,20 +299,18 @@ class SwagEditor extends Component {
               </div>
               <img
                 id="playerImg"
-                src="./assests/img/players/player (1).svg"
-                alt="Your Player"
+                src={null}
+                alt="playerImg"
                 className="hidden"
               />
             </section>
 
-            <section className="flex justify-between bg-red-400">
-              {/* frames */}
-
-              <span className="bg-red-600 p-3 w-40">Swag frames</span>
+            {/* <section className="flex justify-between bg-purple-800">
+              <span className="bg-purple-900 p-3 min-w-200 w-40">Swag frames</span>
               <select
                 onChange={this.handleFrame}
                 name="frameImg"
-                className=" bg-red-400  p-3 select:bg-red-600 customselect"
+                className=" bg-purple-800  p-3 select:bg-purple-900 customselect"
               >
                 <option value="1">Frame1</option>
                 <option value="2">Frame2</option>
@@ -327,39 +318,19 @@ class SwagEditor extends Component {
               </select>
 
               <div className=" flex">
-                {/* <div className="flex bg-red-400 hover:bg-red-800">
-                  <label className="flex flex-col items-center px-4 py-2  bg-transparent  cursor-pointer outline-none focus:outline-none  hover:text-blue-100">
-                    <svg
-                      className="w-8 h-8"
-                      fill="currentColor"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                    </svg>
-                    <input
-                      className="hidden"
-                      type="file"
-                      id="frameUploade"
-                      onChange={() => {
-                        this.imageImportFunction("frameUploade", "frameImg");
-                      }}
-                    />
-                  </label>
-                </div> */}
-                <img id="frameImg" src="./assests/img/background/bg (1).png" alt="Your frame" className="hidden" />
+                <img id="frameImg" src="./swageditor/assests/img/background/bg (1).png" alt="Your frame" className="hidden" />
               </div>
 
-            </section>
+            </section> */}
 
-            <section className="flex justify-between bg-red-400">
+            <section className="flex  bg-purple-800 pr-2">
               {/* Player Position */}
 
-              <span className="bg-red-600 p-3 w-40">Player Position</span>
+              <span className="bg-purple-900 p-3 min-w-200 text-left">Logo Position</span>
               <select
                 onChange={this.handle}
                 name="position"
-                className="bg-red-400  p-3 select:bg-red-600 customselect"
+                className="bg-purple-800 w-full p-3 select:bg-purple-900 customselect"
               >
                 <option value="top">Top</option>
                 <option value="left">Left</option>
@@ -373,13 +344,13 @@ class SwagEditor extends Component {
               </select>
             </section>
 
-            <section className="flex justify-between bg-red-400">
+            <section className="flex bg-purple-800 pr-2">
               {/* Post type */}
-              <span className="bg-red-600 p-3">Post type</span>
+              <span className="bg-purple-900 p-3 min-w-200 text-left">Post type</span>
               <select
                 onChange={this.sizehandle}
                 name="position"
-                className="bg-red-400 p-3"
+                className="bg-purple-800 p-3 w-full"
               >
                 <option value="Logo cover">Logo</option>
                 <option value="Instagram Post">Instagram Post</option>
@@ -391,16 +362,24 @@ class SwagEditor extends Component {
               </select>
             </section>
 
+            <section className="flex  bg-purple-800">
+              {/* Post type */}
+              <span className="bg-purple-900 p-3 min-w-200 text-left">Border Radius</span>
+              <input
+                type="number"
+                placeholder="0"
+                onChange={this.roundedCorner}
+                name="position"
+                className="bg-purple-800 p-3 w-20"
+              />
+            </section>
           </section>
 
-
-
-
           {/* <section className="flex ">
-            <div className="bg-red-400 flex justify-between ">
+            <div className="bg-purple-800 flex justify-between ">
               <select
                 name="position"
-                className=" bg-red-400  p-3 select:bg-red-600 customselect"
+                className=" bg-purple-800  p-3 select:bg-purple-900 customselect"
                 onChange={(e) => {
                   this.setState({ rounded: e.target.value === "true" });
                 }}
@@ -411,9 +390,9 @@ class SwagEditor extends Component {
             </div>
           </section> */}
 
-          <section className="my-4 text-left ml-4 ml-12">
+          <section className="mt-4 mb-10 text-left ml-4 ml-12 flex justify-center">
             <a
-              className="bg-red-600 px-4 py-2 hover:shadow-lg hoverBg"
+              className="bg-purple-900 px-4 py-2 hover:shadow-lg hoverBg h-full"
               id="download"
               onClick={this.download_img}
               download="myImage.jpg"
@@ -423,7 +402,7 @@ class SwagEditor extends Component {
             </a>
 
             <input
-              className="bg-red-400 font-sm  p-2 hover:shadow-lg hoverBg"
+              className="bg-purple-800 font-sm p-2 hover:shadow-lg hoverBg"
               type="button"
               onClick={() => {
                 start && this.setState({ start: false });
@@ -431,8 +410,6 @@ class SwagEditor extends Component {
               }}
               value={start ? "Add Design" : "Update Design"}
             />
-
-
           </section>
 
           <section
@@ -441,8 +418,7 @@ class SwagEditor extends Component {
             style={{ maxWidth: "100%", width: "fit-content", transformOrigin: "0 0" }}
           >
             <canvas
-              className={`border-2`}
-
+              className="border-2 border-purple-800"
               id="backgroundCanvas"
               style={{
                 zIndex: 1,
@@ -453,8 +429,6 @@ class SwagEditor extends Component {
               Canvas.
             </canvas>
           </section>
-
-
         </div>
 
         {customSize && (
@@ -465,12 +439,12 @@ class SwagEditor extends Component {
             <div className="relative">
               <p className="text-center font-medium text-gray-500">
                 Custom dimensions
-            </p>
+              </p>
               <span className="absolute font-medium right-0 text-gray-600 top-0 cursor-pointer" onClick={() => this.setState({
                 customSize: false,
               })}>
                 X
-            </span></div>
+              </span></div>
             <div className="flex ">
               <span className="border-2 m-2  p-2 rounded">
                 <input
@@ -498,7 +472,7 @@ class SwagEditor extends Component {
               <input
                 type="button"
                 value="Submit"
-                className="border-2 bg-red-600 px-3 py-1  w-full m-2 text-white my-4 rounded"
+                className="border-2 bg-purple-900 px-3 py-1  w-full m-2 text-white my-4 rounded"
                 onClick={() => {
                   this.customSizeHandle();
                 }}
